172f2f55af3b36528d52b23854caf200179fa122
virginieVachet <vachetvirginie@gmail.com>
2024-02-02 10:19:56 +0100

    add types

--- dist/my-toto-ui-library.d.ts
+++ dist/my-toto-ui-library.d.ts
@@ -1,3 +1,51 @@
 declare module 'my-toto-ui-library' {
   export function myFunction(): void;
+
+  //import types
+  import { VueConstructor, PluginFunction } from 'vue';
+  import { Framework } from 'vuetify/types';
+
+  // Types for the library usage
+  // eg. Vue.use(MyUILibrary, options)
+
+  export interface Icons {
+      [key: string]: string;
+  }
+
+  export interface Theme {
+      icons: Icons;
+  }
+
+  export interface LocalStorageControl {
+      version?: number;
+      expiration?: number;
+      prefix?: string;
+  }
+
+  export interface MyUILibraryOptions {
+      theme?: Theme;
+      localStorageControl: LocalStorageControl;
+  }
+
+  export interface MyUILibraryPlugin {
+      install: PluginFunction<MyUILibraryOptions>;
+  }
+
+  declare const MyUILibrary: MyUILibraryPlugin;
+
+  declare global {
+    interface Window {
+      Vue?: VueConstructor;
+    }
+  }
+
+    declare module 'vue/types/vue' {
+        interface Vue {
+        $vd: MyUILibraryOptions;
+        $vuetify: Framework;
+        }
+    }
+
+    export default MyUILibrary;
+
 }
